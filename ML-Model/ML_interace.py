ea7831d7a3df8b4767f6b9107cb0690e47062630
Egorchik <tchepasovegor@gmail.com>
2026-06-11 14:40:53 +0300

    Сделан промежуточный отчет, а так же добавлены параметры которые будут использованы для обучения модели

--- ML-Model/ML_interace.py
+++ ML-Model/ML_interace.py
@@ -2,6 +2,34 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, mean_absolute_error, mean_squared_error
 
+import numpy as np
+import pandas as pd
+
+data = pd.read_csv("benchmark_results.csv")
+
+# После того, как данные будут считаны, нужно будет разделить их на обучающую и тестовую выборки
+
+feature_columns = [
+    "approach",
+    "D",
+    "W",
+    "F",
+    "P_read",
+    "P_write",
+    "P_mkdir",
+    "P_ls",
+    "P_mv",
+    "P_find",
+    "Dist",
+    "Loc"
+]
+
+target_columns = [
+    "avg_latency",
+    "p99_latency",
+    "memory",
+    "throughput"
+]
 
 model = RandomForestRegressor(
     n_estimators=300,
